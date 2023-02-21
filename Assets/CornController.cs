8ff47bb8f306b135088aa2f05c834b32650173e1
Tontoppy <tomoki0nakayama@gmail.com>
2023-02-22 00:06:33 +0900

    基礎課題まで

--- Assets/CornController.cs
+++ Assets/CornController.cs
@@ -2,19 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoinController : MonoBehaviour
+public class CornController : MonoBehaviour
 {
     //Unityちゃんのtransform
     private Transform unitychanTransform;
     //このオブジェクトのtransform
     private Transform myTransform;
-
     //Unityちゃんとの距離限界（これ以上Unityちゃんの後ろに行くとオブジェクトを破壊）
     private int limitDistanse = 6;
+
     // Start is called before the first frame update
     void Start()
     {
-        this.transform.Rotate(0, Random.Range(0, 360), 0);
         this.myTransform = this.transform;
         this.unitychanTransform = GameObject.Find("unitychan").transform;
     }
@@ -22,12 +21,11 @@ public class CoinController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.transform.Rotate(0, 3, 0);
-
-        if(this.myTransform.position.z +limitDistanse <= this.unitychanTransform.position.z)
+        if (this.myTransform.position.z + limitDistanse <= this.unitychanTransform.position.z)
         {
             Destroy(this.gameObject);
         }
 
-    }
+    }   // Start is called before the first frame update
+
 }
